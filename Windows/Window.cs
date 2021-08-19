9d8ba251edfd2a75a80d029d0505507919d6d48a
DoctorRidley <bckoshy97@gmail.com>
2021-08-18 20:01:26 -0700

    Window classes now properly inherit from a base class

---
+++ Windows/Window.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Classes.Windows {
+    public abstract class Window : System.Windows.Window {
+
+        private readonly string name;
+        private readonly string route;
+
+        public Window(string name, string route) {
+            this.name = name;
+            this.route = route;
+
+        }
+
+        public string Name => name;
+
+        public string Route => route;
+
+    }
+}

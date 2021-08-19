98848ba2fcba6a259ef87c91d168fbf4b34a1d3b
DoctorRidley <bckoshy97@gmail.com>
2021-08-18 19:58:26 -0700

    Moved Router and State classes to Utilities directory.

---
+++ Utilities/Router.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Classes.Windows;
+
+
+namespace Classes.Utilities {
+
+
+    enum Window_State { WELCOME, LOGIN, REGISTER, DASHBOARD, PROFILE, STUDENT_CLASS, FACULTY_CLASS };
+
+
+    public class Router {
+
+        private Window_State current_page;
+
+        private readonly Stack<Window> history = new();
+        private readonly List<Window> windows = new();
+
+        public Router() {
+
+        }
+
+        public void Add_Window(Window w) { windows.Add(w); }
+
+        public Window Get_Current_Page() => windows[(int)current_page];
+
+
+        // foreach Window_State, return the corresponding Window in windows<>
+        private Window Get_Window(Window_State e) => windows[(int)e];
+
+
+        public void Navigate(string route) {
+
+            // push current page to history
+
+        }
+
+        public void Previous_Window() {
+            //FIXME: handle empty history
+            Navigate(history.Pop().Route);
+        }
+    }
+}
---
+++ Utilities/State.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Classes.Windows;
+
+namespace Classes.Utilities {
+
+    class State {
+
+        private bool app_running;
+        private readonly Router router = new();
+
+        public State() {
+            app_running = true;
+            //router.Add_Window(new Welcome());
+        }
+
+
+
+
+        public void Terminate() {
+            app_running = false;
+        }
+
+
+
+    }
+}

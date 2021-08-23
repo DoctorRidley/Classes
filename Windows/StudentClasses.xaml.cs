ae023424e2e277049319e6dededfd08f9c29f6dd
DoctorRidley <bckoshy97@gmail.com>
2021-08-23 13:08:30 -0700

    Created window for students to add a class

---
+++ Windows/StudentClasses.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Classes.Windows {
+    /// <summary>
+    /// Interaction logic for StudentClasses.xaml
+    /// </summary>
+    public partial class StudentClasses : Window {
+
+        public StudentClasses() : base("Class Registration", "student-register") {
+            InitializeComponent();
+        }
+    }
+}

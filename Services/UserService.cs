7172133707bfd9365a157f2dee1f9e3e272d8543
DoctorRidley <bckoshy97@gmail.com>
2021-08-26 23:33:40 -0700

    Created class to help handle user queries to DB

---
+++ Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+
+using Classes.Repos;
+using Classes.Models;
+
+namespace Classes.Services {
+
+    class UserService {
+
+        private readonly StudentRepo student_repo;
+
+        public UserService(StudentRepo s) => student_repo = s;
+
+        public Person Login(string username, string password) {
+
+        }
+
+
+        private byte[] Hash_Password(string password) {
+
+            //byte[] = System.Convert.ToByte(password);
+            //byte[] hash = SHA512.Create().ComputeHash(password);
+        }
+    }
+
+    
+}

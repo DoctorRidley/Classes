ea65bb63e2c9eb6aac1db3394726f57da437c4af
DoctorRidley <bckoshy97@gmail.com>
2021-08-28 04:06:19 -0700

    Added some abstract methods to Repo interface

--- Repos/ICrudRepo.cs
+++ Repos/ICrudRepo.cs
@@ -4,9 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+using Classes.Models;
+
 namespace Classes.Repos {
     
     public interface ICrudRepo {
+
+        public void Create(Person new_user);
+
+        public Person Find_By_Username(string username);
+
+        public void Update(Person person);
+
+        public void Delete(Person person);
+
+        
 
     }
 }
